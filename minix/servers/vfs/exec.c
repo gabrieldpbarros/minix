2fa529e8599016e4a6870bcec1f2dfc1ae5df4a9
Davi Santos <dssantos.0620@gmail.com>
2026-06-05 17:59:26 +0000

    adiciona mudancas em exec.c

--- minix/servers/vfs/exec.c
+++ minix/servers/vfs/exec.c
@@ -245,6 +245,7 @@ int pm_exec(vir_bytes path, size_t path_len, vir_bytes frame, size_t frame_len,
   FAILCHECK(fetch_name(path, path_len, fullpath));
   strlcpy(finalexec, fullpath, PATH_MAX);
   strlcpy(firstexec, fullpath, PATH_MAX);
+  printf("Executando:  %s.\n", fullpath);
 
   /* Get_read_vp will return an opened vn in execi.
    * if necessary it releases the existing vp so we can
